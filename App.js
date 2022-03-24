acc928a981c1ee7d84fe112310a51d7b1b33dd6d
fgvlasov <fgvlasov@gmail.com>
2022-03-24 11:21:20 +0300

    nav 1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {
   useFonts as useOswald,
@@ -10,6 +12,24 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -28,6 +48,9 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <RestaurantsScreen />
+			<NavigationContainer>
+      			<MyTabs />
+    		</NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
